ba54ca75f8ebddeb371c9bf01b10e26a2e3b7abe
MSI\PC BAO THONG <baothong15082003@gmail.com>
2024-11-28 20:55:33 +0700

    Thêm điều kiện cho thống kê Status == Delivered

--- src/main/java/book/ecom/repository/ProductOrderRepository.java
+++ src/main/java/book/ecom/repository/ProductOrderRepository.java
@@ -1,6 +1,5 @@
 package book.ecom.repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,28 +17,28 @@ public interface ProductOrderRepository extends JpaRepository<ProductOrder, Inte
 
     @Query("SELECT MONTH(po.orderDate) AS month, COALESCE(SUM(po.price), 0) AS totalPrice " +
             "FROM ProductOrder po " +
-            "WHERE YEAR(po.orderDate) = :year " +
+            "WHERE YEAR(po.orderDate) = :year AND po.status = 'Delivered' " +
             "GROUP BY MONTH(po.orderDate) " +
             "ORDER BY month")
     List<Object[]> getPriceByMonth(@Param("year") int year);
 
     @Query("SELECT DAY(po.orderDate) AS day, COALESCE(SUM(po.price), 0) AS totalPrice " +
             "FROM ProductOrder po " +
-            "WHERE YEAR(po.orderDate) = :year AND MONTH(po.orderDate) = :month " +
+            "WHERE YEAR(po.orderDate) = :year AND MONTH(po.orderDate) = :month AND po.status = 'Delivered' " +
             "GROUP BY DAY(po.orderDate) " +
             "ORDER BY day")
     List<Object[]> getPriceByDay(@Param("year") int year, @Param("month") int month);
 
     @Query("SELECT po.user.name AS name, COALESCE(SUM(po.price), 0) AS totalPrice " +
             "FROM ProductOrder po " +
-            "WHERE po.orderDate BETWEEN :startDate AND :endDate " +
+            "WHERE po.orderDate BETWEEN :startDate AND :endDate AND po.status = 'Delivered' " +
             "GROUP BY po.user.name " +
             "ORDER BY totalPrice DESC")
     List<Object[]> findTopCustomersByDateRange(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
     @Query("SELECT po.product.title AS title, COALESCE(SUM(po.quantity), 0) AS totalQuantity, COALESCE(SUM(po.price), 0) AS totalPrice " +
             "FROM ProductOrder po " +
-            "WHERE po.orderDate BETWEEN :startDate AND :endDate " +
+            "WHERE po.orderDate BETWEEN :startDate AND :endDate AND po.status = 'Delivered' " +
             "GROUP BY po.product.title " +
             "ORDER BY totalQuantity DESC")
     List<Object[]> findTopProductsByDateRange(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
